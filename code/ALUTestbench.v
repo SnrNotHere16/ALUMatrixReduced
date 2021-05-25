31dd7d03b880080e60af8acc109470feb7a6d791
SnrNotHere16 <aquineda16@hotmail.com>
2021-05-24 20:10:21 -0700

    Initial begin works
    sel - 18 will set eleOut to the value of G00 (32'h0)

--- code/ALUTestbench.v
+++ code/ALUTestbench.v
@@ -1,4 +1,25 @@
 `timescale 1ns / 1ps
 
 module ALUTestbench();
+reg clk, reset; 
+reg [5:0] sel; 
+reg [31:0] eleIn; 
+wire [31:0] eleOut; 
+ALUMatrixTop DUT
+      (
+        .clk(clk), 
+        .reset(reset), 
+        .sel(sel), 
+        .eleIn(eleIn), 
+        .eleOut(eleOut)
+       );
+       
+always #5 clk = ~clk;
+    initial begin 
+       clk  = 0;
+       reset  = 1;
+       sel = 18; 
+       #50 reset  = 0;
+     end
+    
 endmodule
