74458fc1bc9c8936228e8dbf829edaa4e2726a90
SnrNotHere16 <aquineda16@hotmail.com>
2021-05-25 10:18:00 -0700

    Give Matrix A and B values
    Set the values of matrix A and B

--- code/ALUTestbench.v
+++ code/ALUTestbench.v
@@ -29,6 +29,24 @@ always #5 clk = ~clk;
        clk  = 0;
        reset  = 1;
        #50 reset  = 0;    
-       dummy = select(18,30); 
+       dummy = select(18,30);
+       #20 dummy = select (0,0);  
+       #20 dummy = select (1,1);  
+       #20 dummy = select (2,2);
+       #20 dummy = select (3,3);  
+       #20 dummy = select (4,4);  
+       #20 dummy = select (5,5);
+       #20 dummy = select (6,6);  
+       #20 dummy = select (7,7);  
+       #20 dummy = select (8,8);
+       #20 dummy = select (9,0);  
+       #20 dummy = select (10,1);  
+       #20 dummy = select (11,2);
+       #20 dummy = select (12,3);  
+       #20 dummy = select (13,4);  
+       #20 dummy = select (14,5);
+       #20 dummy = select (15,6);  
+       #20 dummy = select (16,7);  
+       #20 dummy = select (17,8);
      end 
 endmodule
