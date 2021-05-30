7e7e22194ca347746bb7fc854a845f13f2cf594d
SnrNotHere16 <aquineda16@hotmail.com>
2021-05-30 14:23:59 -0700

    Added the new output and input
    output - LED 
    input - eleSel - selects element to output onto LED

--- code/ALUMatrixTop.v
+++ code/ALUMatrixTop.v
@@ -4,14 +4,16 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module ALUMatrixTop(clk, reset, sel, eleIn, eleOut);
+module ALUMatrixTop(clk, reset, eleSel, sel, eleIn, eleOut, LED);
 
-input clk, reset; 
-input  [5:0] sel; 
+input clk, reset;
+input [4:0] eleSel;  
+input  [5:0] sel;
 input  [31:0] eleIn; 
-output [31:0] eleOut; 
+output [31:0] eleOut;
+output [15:0] LED;  
 
-     ALUCalc zero(.clk(clk), .reset(reset), .sel(sel), 
-                 .eleIn(eleIn), .eleOut(eleOut)); 
+     ALUCalc zero(.clk(clk), .reset(reset), .eleSel(eleSel),.sel(sel), 
+                 .eleIn(eleIn), .eleOut(eleOut), .LED(LED)); 
     
 endmodule
