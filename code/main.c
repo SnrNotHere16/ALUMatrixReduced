ec9306a194c5a43a3fd5dcf3bb450dfd105c107b
SnrNotHere16 <aquineda16@hotmail.com>
2021-05-30 13:50:00 -0700

    Update main.c

--- code/main.c
+++ code/main.c
@@ -3,8 +3,12 @@
 int main()
 {
 	initGPIO();
-	printMatrA();
-	printMatrB();
+	printMatrA(0,1,2,3,4,5,6,7,8);
+	printMatrB(0,1,2,3,4,5,6,7,8);
+
+	initMatrA(0,1,2,3,4,5,6,7,8);
+	transpose();
+	printMatrC();
 
 
 }
