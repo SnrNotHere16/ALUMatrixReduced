7b9a70cded2e4833a03b171de2d0af00ea9ddc5a
SnrNotHere16 <aquineda16@hotmail.com>
2021-05-31 13:38:29 -0700

    Subtraction does not work nor transpose (14)

--- code/main.c
+++ code/main.c
@@ -2,13 +2,23 @@
 
 int main()
 {
+	int j = 0;
 	initGPIO();
+	resetMatr();
 	printMatrA(0,1,2,3,4,5,6,7,8);
 	printMatrB(0,1,2,3,4,5,6,7,8);
-
 	initMatrA(0,1,2,3,4,5,6,7,8);
 	transpose();
 	printMatrC();
+	initMatrB(0,1,2,3,4,5,6,7,8);
+	transpose();
+	xil_printf("d d d\n");
+	printMatrC();
+	addition();
+	printMatrC();
+	subtraction();
+	printMatrC();
+
 
 
 }
