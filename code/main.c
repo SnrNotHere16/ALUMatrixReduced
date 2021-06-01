1e374fe1fc211cdafa2f9dfe63b9b77e26ffd265
SnrNotHere16 <aquineda16@hotmail.com>
2021-06-01 12:00:12 -0700

    Update 1.a
    Transpose, addition, subtraction working 
    No user input

--- code/main.c
+++ code/main.c
@@ -5,7 +5,6 @@ int main()
 	int j = 0;
 	initGPIO();
 	resetMatr();
-	printMatrA(0,1,2,3,4,5,6,7,8);
 	initMatrA(0,1,2,3,4,5,6,7,8);
 	initMatrB(0,1,2,3,4,5,6,7,8);
 	transpose();
@@ -24,15 +23,4 @@ int main()
 	transpose();
 	printMatrC();
 	*/
-	/*
-	xil_printf("d d d\n");
-	printMatrC();
-	addition();
-	printMatrC();
-	subtraction();
-	printMatrC();
-	*/
-
-
-
 }
