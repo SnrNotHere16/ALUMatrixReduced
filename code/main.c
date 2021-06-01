460867e7803e215873f6afdb9f35c75d3038fbf1
SnrNotHere16 <aquineda16@hotmail.com>
2021-06-01 11:29:32 -0700

    Update 1
    Working - 
    Transpose 
    Subtraction 
    Addition 
    initMatrA does not take user input

--- code/main.c
+++ code/main.c
@@ -6,18 +6,32 @@ int main()
 	initGPIO();
 	resetMatr();
 	printMatrA(0,1,2,3,4,5,6,7,8);
-	printMatrB(0,1,2,3,4,5,6,7,8);
 	initMatrA(0,1,2,3,4,5,6,7,8);
-	transpose();
-	printMatrC();
 	initMatrB(0,1,2,3,4,5,6,7,8);
 	transpose();
+	printMatrC();
+//	initMatrA(0,1,2,3,4,5,6,7,8);
+//	initMatrB(0,1,2,3,4,5,6,7,8);
+	//transpose();
+	printMatrC();
+	printMatrC();
+	addition();
+	printMatrC();
+	subtraction();
+	printMatrC();
+	/*
+	initMatrB(0,1,2,3,4,5,6,7,8);
+	transpose();
+	printMatrC();
+	*/
+	/*
 	xil_printf("d d d\n");
 	printMatrC();
 	addition();
 	printMatrC();
 	subtraction();
 	printMatrC();
+	*/
 
 
 
