c951bbed004bc03ccc1bc2ea1f3ac9e318d7700a
SnrNotHere16 <aquineda16@hotmail.com>
2021-06-01 12:06:00 -0700

    update 1.b
    Transpose, addition, subtraction , multiplication working 
    No user input

--- code/main.c
+++ code/main.c
@@ -9,18 +9,10 @@ int main()
 	initMatrB(0,1,2,3,4,5,6,7,8);
 	transpose();
 	printMatrC();
-//	initMatrA(0,1,2,3,4,5,6,7,8);
-//	initMatrB(0,1,2,3,4,5,6,7,8);
-	//transpose();
-	printMatrC();
-	printMatrC();
 	addition();
 	printMatrC();
 	subtraction();
 	printMatrC();
-	/*
-	initMatrB(0,1,2,3,4,5,6,7,8);
-	transpose();
+	multiplication();
 	printMatrC();
-	*/
 }
