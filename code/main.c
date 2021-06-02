51bc8b9a90ee0be87d2455cd150a2b2f840c56e7
SnrNotHere16 <aquineda16@hotmail.com>
2021-06-01 19:56:41 -0700

    Update 2.a
    Added new  matrices

--- code/main.c
+++ code/main.c
@@ -4,8 +4,11 @@ int main()
 {
 	initGPIO();
 	resetMatr();
-	initMatrA(0,1,2,3,4,5,6,7,8);
-	initMatrB(0,1,2,3,4,5,6,7,8);
+	//initMatrA(0,1,2,3,4,5,6,7,8);
+	//initMatrA(8,7,6,5,4,3,2,1,0);
+	//initMatrB(0,1,2,3,4,5,6,7,8);
+	initMatrA(20,11,4,-5,3,4,8,-100,25);
+	initMatrB(1,0,0,0,1,0,0,0,1);
 	transpose();
 	printMatrC();
 	addition();
@@ -14,7 +17,8 @@ int main()
 	printMatrC();
 	multiplication();
 	printMatrC();
-	scalarMult(2);
+	//scalarMult(2);
+	scalarMult(-4);
 	printMatrC();
 	determinant();
 }
