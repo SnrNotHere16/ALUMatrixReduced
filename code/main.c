9dd3db0afec53c81a6048ee0d503b0f7ea76bee9
SnrNotHere16 <aquineda16@hotmail.com>
2021-06-01 20:24:55 -0700

    Verified outputs from TB (update 3.a)
    Added two new matrices 
    (0,2,2,3,4,8,6,17,18)
    (10,11,12,3,4,5,6,7,0)

--- code/main.c
+++ code/main.c
@@ -7,8 +7,10 @@ int main()
 	//initMatrA(0,1,2,3,4,5,6,7,8);
 	//initMatrA(8,7,6,5,4,3,2,1,0);
 	//initMatrB(0,1,2,3,4,5,6,7,8);
-	initMatrA(20,11,4,-5,3,4,8,-100,25);
-	initMatrB(1,0,0,0,1,0,0,0,1);
+	//initMatrA(20,11,4,-5,3,4,8,-100,25);
+	//initMatrB(1,0,0,0,1,0,0,0,1);
+	initMatrA(0,2,2,3,4,8,6,17,18);
+	initMatrB(10,11,12,3,4,5,6,7,0);
 	transpose();
 	printMatrC();
 	addition();
